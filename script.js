fe4f900c2a0b6ba53361f36955d1731d173c0101
Hasan Al Mahmud <h.a.mahmud1519@gmail.com>
2024-12-20 19:20:38 +0600

    added logic to play a single round and prompt for invalid choice

--- script.js
+++ script.js
@@ -13,6 +13,29 @@ function getComputerChoice() {
 }
 // logic to get the human choice
 function getHumanChoice() {
-    let userChoice = prompt("Enter your choice: rock, paper or scissors");
-    return userChoice.toLowerCase();
+    let userChoice = prompt("Enter your choice: rock, paper or scissors").toLowerCase();
+    // ask for a valid choice
+    while (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors") {
+        userChoice = prompt("Invalid choice! Please enter rock, paper or scissor").toLowerCase();
+    }
+    return userChoice;
+}
+
+// logic to play a single round
+function playSingleRound(humanChoice, computerChoice) {
+    console.log(`You chose: ${humanChoice}`);
+    console.log(`computer chose: ${computerChoice}`);
+    
+    if ((humanChoice === "rock" && computerChoice === "scissors") || 
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")    
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        
+    }else if (humanChoice === computerChoice) {
+        console.log(`It's a tie!`);
+    }else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        
+    }
 }
